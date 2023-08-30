5d17a0e455e4e933fda47890d9d304e6ade2dfa9
marcospet <marcos.sousa@petlove.com.br>
2023-08-30 20:37:58 -0300

    feat: Ajsute proteção rotas

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,19 @@ export default async function middleware(req: NextRequest) {
     const token = req.cookies.get("erp.token")?.value
     console.log(  token );
 
-    if (pathname === '/' && token === undefined) {
+   
+    if (pathname === '/' && token === undefined ) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+     
+    if (token === undefined  && pathname !== '/' && pathname !== '/login' ) {
       return NextResponse.redirect(new URL('/login', req.url));
     }
   
-    if (pathname === '/login') {
+    if (pathname === '/login' && token) {
       return NextResponse.redirect(new URL('/dashboard', req.url));
     }
-    if (pathname === '/') {
+    if (pathname === '/' && token) {
       return NextResponse.redirect(new URL('/dashboard', req.url));
     }
   
