2c94ff7ac8bd577770a5edf633149987b95fe369
marcospet <marcos.sousa@petlove.com.br>
2023-09-13 19:32:13 -0300

    refactor: Adicionado data attributes para melhora a verificação

--- app/(authenticated)/components/layout/Sidebar.tsx
+++ app/(authenticated)/components/layout/Sidebar.tsx
@@ -7,26 +7,26 @@ import { Nav } from "../nav";
 
 
 export default function Sidbar() {
+  
   const path = usePathname();
-  console.log(path);
   
   return (
     <div className="relative hidden h-screen shadow-lg lg:block w-80">
       <div className="h-full bg-white dark:bg-gray-700">
         <div className="flex items-center justify-start pt-6 ml-8">
-          <p className="text-xl font-bold dark:text-white">Plannifer</p>
+          <p className="text-xl font-bold dark:text-white">ERP</p>
         </div>
         <Nav.Root>
           <Nav.Link 
             route='/dashboard'
-            isActive={path ==='/dashboard'}
+            active={path ==='/dashboard'}
             >
             <Nav.Icon icon={<BiSolidDashboard size={20}/>}/>
             <Nav.IconLabel label='Dashboard'/>
           </Nav.Link>
           <Nav.Link 
             route='/registre'
-            isActive={path ==='/registre'}
+            active={path ==='/registre'}
             >
             <Nav.Icon icon={<MdLibraryBooks size={20}/>}/>
             <Nav.IconLabel label='Cadastro'/>
--- app/(authenticated)/components/nav/NavLink.tsx
+++ app/(authenticated)/components/nav/NavLink.tsx
@@ -1,32 +1,35 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+
+
 type Props = {
-    children: ReactNode,
-    route: string,
-    isActive: boolean
+  children: ReactNode,
+  route: string,
+  active: boolean
 }
 
-export function NavLink({children, route, isActive }:Props) {
+
+export function NavLink({children, route, active }:Props) {
   return (
     <>
       <Link
         href={route}
-        className={`flex items-center 
-        justify-start
-        w-full 
-        p-2 
-        pl-6 
-        my-2
-        ${isActive ?  
-          'text-gray-800 transition-colors duration-200 border-l-4 border-purple-500 dark:text-white' :
-          'text-gray-400 transition-colors duration-200 border-l-4 border-transparent hover:text-gray-800'
-        } `}
+        data-active={active}
+        className='flex 
+        items-center 
+        justify-start 
+        w-full p-2 
+        pl-6 my-2 
+        text-gray-400
+        transition-colors duration-200
+        border-l-4 border-transparent 
+        hover:text-gray-800
+        data-[active=true]:text-gray-800
+        data-[active=true]:border-purple-500
+        data-[active=true]:dark:text-white'
       >
         {children}
       </Link>
     </>
-     
-
-  
   )  
 } 
